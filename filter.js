9d170f4b98c29305517563ad97d55fcbf176c24e
blankonxkonde <69258970+blankonxkonde@users.noreply.github.com>
2023-07-16 17:53:09 +0700

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,11 +1,11 @@
-function myFunction(inputId, column) {
+function myFunction() {
     var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById(inputId);
+    input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
     table = document.getElementById("myTable");
     tr = table.getElementsByTagName("tr");
     for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[column];
+        td = tr[i].getElementsByTagName("td")[0];
         if (td) {
             txtValue = td.textContent || td.innerText;
             if (txtValue.toUpperCase().indexOf(filter) > -1) {
